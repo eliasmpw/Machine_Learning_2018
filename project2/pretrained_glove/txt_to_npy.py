8185208633db6b211b0e5701f813d53e1e5b1fea
Jonas Jäggi <jonas.jaeggi@gmail.com>
2018-11-26 15:41:24 +0100

    add readme to trained glove

--- project2/pretrained_glove/txt_to_npy.py
+++ project2/pretrained_glove/txt_to_npy.py
@@ -1,35 +1,36 @@
 import numpy as np
+import pickle
+
+
+
 
 def main():
-    filename = 'glove.twitter.27B.50d.txt'
-    
-    embeddings = np.zeros((0, 50))
+    filename = 'glove.twitter.27B.25d.txt'
+    dimensions = 25
+
+    with open('../vocab.pkl', 'rb') as f:
+        vocab = pickle.load(f)
+
+    vocab_reduced = dict()
+    embeddings = []
+    index = 0
     with open(filename) as f:
         for line in f:
             itervalues = iter(line.strip().split())
-            next(itervalues)
-            vec = np.asarray([])
-            for word in itervalues:
-                vec.append(float(word))
-            embeddings.append(vec, axis=0)
-    embeddings = np.asarray(embeddings)
-    print(embeddings.shape)
-    print(embeddings[1:3, :])
+            word = next(itervalues)
+            if word in vocab:
+                vocab_reduced[word] = index
+                index += 1
+                vec = []
+                for value in itervalues:
+                    embeddings.append(float(value))
+ 
+    embeddings = np.asarray(embeddings).reshape((-1, dimensions))
+    np.save('embeddings' + str(dimensions), embeddings)
 
+    with open('vocab_pretrained.pkl', 'wb') as f:
+        pickle.dump(vocab_reduced, f, pickle.HIGHEST_PROTOCOL)
 
-
-'''
-def pickle_vocab(vocab_name):
-    vocab = dict()
-    with open('vocab_cut.txt') as f:
-        for idx, line in enumerate(f):
-            vocab[line.strip()] = idx
-
-    with open('vocab.pkl', 'wb') as f:
-        filehandler = open(vocab_name,"wb")
-        pickle.dump(vocab, f, filehandler)
-****
-'''
 
 if __name__ == '__main__':
     main()
